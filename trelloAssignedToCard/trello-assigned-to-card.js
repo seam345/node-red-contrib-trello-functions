614f368c91e9ad5eabf9acd9e66647afdc768f4b
sean borg <sean_borg@hotmail.co.uk>
2019-01-18 12:19:48 +0000

    Added exclude self created to Assign-to-card, also alinged the code to match other nodes
    Code aligning included;
    Retriving the information from the trello actions insted of retreiving a list of assigned cards and calculating the difference from last check.
    The HTML setup now correctly loads the currently selected Board ID and List ID.
    Organising the javscript into self contained methods
    Refactoring var to let and const

--- trelloAssignedToCard/trello-assigned-to-card.js
+++ trelloAssignedToCard/trello-assigned-to-card.js
@@ -3,84 +3,188 @@ var Trello = require('node-trello');
 module.exports = function (RED) {
   function trelloAssignedToCardNode(config) {
     RED.nodes.createNode(this, config);
-    var node = this;
-    var nodeContext = this.context();
+    const node = this;
+    const nodeContext = this.context();
 
     // Retrieve the config node
-    var credentialNode = RED.nodes.getNode(config.trello);
-    var trello = new Trello(credentialNode.apikey, credentialNode.token);
+    const credentialNode = RED.nodes.getNode(config.trello);
+    const trello = new Trello(credentialNode.apikey, credentialNode.token);
     this.on('input', function (msg) {
-      const ID_ACCESS_STRING = 'idSet';
-      var trelloData = msg.trello || {};
-      var listId = trelloData.idList || config.idList;
-      var boardId = trelloData.idBoard || config.idBoard;
-
-      var previousIdSet = null;
+      //<editor-fold desc="Initialise all variables">
+      const trelloCurrentUserIDSaveString = 'trelloUID';
+      const lastFetchedSaveString = 'lastFetched';
+      const boardFetchesSaveString = 'boardFetches';
+      const listId = config.idList;
+      const boardId = config.idBoard;
+      const excludeSelfCreated = config.excludeUser;
+      let trelloCurrentUserID = '';
       try {
-        previousIdSet = nodeContext.get(ID_ACCESS_STRING);
+        trelloCurrentUserID = nodeContext.get(trelloCurrentUserIDSaveString);
       } catch (e) {
         node.error(e)
       }
+      let lastFetchedISO = '';
+      try {
+        lastFetchedISO = nodeContext.get(lastFetchedSaveString);
+      } catch (e) {
+        node.error(e)
+      }
+      // Board fetches contains a set of timestamps each board was last fetched
+      let boardFetchesDictISO = {};
+      try {
+        boardFetchesDictISO = nodeContext.get(boardFetchesSaveString);
+      } catch (e) {
+        node.error(e)
+      }
+      // The get call doesn't error if empty, it just sets the verible to null so reset it to an empty dict
+      if (!boardFetchesDictISO) {
+        boardFetchesDictISO = {};
+      }
+      if (!lastFetchedISO) {
+        if (msg.payload.initialTimeStamp) {
+          lastFetchedISO = msg.payload.initialTimeStamp;
+          node.warn('Used initialTimeStamp input to set the starting point. This could cause some duplication.');
+        }
+      }
+      //</editor-fold>
 
-      // There is no timestamp to say when the user was added so I hold an id set from the previous call and reference that
-      function outputNewCardsAndSetLastFetched(cardData, boardId, listId) {
-        var previousIdSet = nodeContext.get(ID_ACCESS_STRING);
-        var newIdSet = new Set();
+      // pre check that we have the current user ID associated with the api key, if not we shall go fetch it.
+      // TODO I may want to set a flag if the credentials have changed as that would change the user
+      function getTrelloCurrentUID() {
+        if (trelloCurrentUserID) {
+          main();
+        } else {
+          trello.get('/1/members/me', (err, data) => {
+            if (err) {
+              node.error(err)
+            } else {
+              trelloCurrentUserID = data.id;
+              nodeContext.set(trelloCurrentUserIDSaveString, trelloCurrentUserID); // store for faster access
+              main()
+            }
+          })
+        }
+      }
 
-        for (let i = 0; i < cardData.length; i++) {
-          var id = cardData[i].id;
-
-          if (!previousIdSet.has(id)) {
-            if (boardId) {
-              if (boardId === cardData[i].idBoard) {
-                if (listId) {
-                  if (listId === cardData[i].idList) {
-                    node.send({payload: cardData[i]});
+      // Main method, called after checking current Trello user ID in getTrelloCurrentUID.
+      function main() {
+        const triggeredDate = new Date();
+        if (lastFetchedISO) {
+          if (listId) {
+            trello.get('/1/lists/' + listId + '/actions', {since: lastFetchedISO, before: triggeredDate.toISOString()},
+                (err, data) => {
+                  if (err) {
+                    node.error(err)
+                  } else {
+                    outputNewCards(data);
+                    setLastFetched(triggeredDate, boardId);
+                    node.status({fill: "green", shape: "dot", text: triggeredDate.toISOString().substring(0, 19)});
                   }
-                } else { // there is no list id, send out card payload for all newly assigned cards on board
-                  node.send({payload: cardData[i]});
+                }
+            );
+          } else if (boardId) {
+            trello.get('/1/boards/' + boardId + '/actions', {
+                  since: lastFetchedISO,
+                  before: triggeredDate.toISOString()
+                },
+                (err, data) => {
+                  if (err) {
+                    node.error(err)
+                  } else {
+                    outputNewCards(data);
+                    setLastFetched(triggeredDate, boardId);
+                    node.status({fill: "green", shape: "dot", text: triggeredDate.toISOString().substring(0, 19)});
+                  }
+                }
+            );
+          } else {
+            trello.get('/1/members/me/boards', (err, data) => {
+                  if (err) {
+                    node.error(err)
+                  } else {
+                    for (let i = 0; i < data.length; i++) {
+                      // Here I check to see if any previous boards have been previously missed for any reason, if so I
+                      // output a log message to notify the user; of the board missed, when it was last fetched and that
+                      // the program shall try again from the last failed attempt.
+                      let fetchDatetime = lastFetchedISO;
+                      if (boardFetchesDictISO && boardFetchesDictISO[data[i].id]) {
+                        fetchDatetime = boardFetchesDictISO[data[i].id];
+                        if (fetchDatetime !== lastFetchedISO) {
+                          node.log('Attempting to recover any missed card movements from ' + data[i].name + ': ' +
+                              data[i].id + ' since ' + fetchDatetime)
+                        }
+                      }
+
+                      trello.get('/1/boards/' + data[i].id + '/actions', {
+                            since: fetchDatetime,
+                            before: triggeredDate.toISOString()
+                          },
+                          (err, dataBoard) => {
+                            if (err) {
+                              node.error('Failed to retrieve board ' + data[i].name + ': ' + data[i].id +
+                                  ' shall attempt to recover any missed card movements when next triggered');
+                              node.error(err)
+                            } else {
+                              outputNewCards(dataBoard);
+                              setLastFetched(triggeredDate, data[i].id);
+                              node.status({
+                                fill: "green",
+                                shape: "dot",
+                                text: triggeredDate.toISOString().substring(0, 19)
+                              });
+                            }
+                          }
+                      );
+                    }
+                  }
+                }
+            );
+          }
+        } else {
+          // Set the first last fetched so next time I know the window of time to check for new cards.
+          setLastFetched(triggeredDate);
+        }
+      }
+
+      // Filters action json data and get any cards created, to then be sent as a msg.payload. Called from main.
+      function outputNewCards(actionData) {
+        for (let i = 0; i < actionData.length; i++) {
+          if (actionData[i].data) {
+            if (actionData[i].type === 'addMemberToCard') {
+              if (actionData[i].member.id === trelloCurrentUserID) {  // checks that the member added is the user
+                node.send({payload: actionData[i]});
+                // Bellow will test if excludeSelfCreated is set, if it wasn't it will just continue, if it was it will
+                // check that the user that created the card is not the user who's linked to (owns) the API key
+                if (!excludeSelfCreated || !(actionData[i].idMemberCreator === trelloCurrentUserID)) {
+                  trello.get('/1/cards/' + actionData[i].data.card.id, (err, data) => {
+                        if (err) {
+                          node.error(err)
+                        } else {
+                          node.send({payload: data});
+                        }
+                      }
+                  );
                 }
               }
-            } else { // There is no board id, send out card payload for all newly assigned cards
-              node.send({payload: cardData[i]});
             }
           }
+        }
+      }
 
-          newIdSet.add(id);
+      // Sets last fetched data, Called from main.
+      function setLastFetched(triggeredDate, boardID) {
+        // Set to triggered time so we can be sure not to miss any
+        nodeContext.set(lastFetchedSaveString, triggeredDate.toISOString());
+
+        if (boardID) {
+          boardFetchesDictISO[boardID] = triggeredDate.toISOString();
+          nodeContext.set(boardFetchesSaveString, boardFetchesDictISO);
 
         }
-        nodeContext.set(ID_ACCESS_STRING, newIdSet);
-        node.status({fill: "green", shape: "dot", text: new Date().toISOString().substring(0, 19)});
-      }
-
-      if (!previousIdSet) {
-        // todo add ability to have a predefined set
       }
 
 
-      if (previousIdSet) {
-        trello.get('/1/members/me/cards', (err, data) => {
-              if (err) {
-                node.error(err)
-              } else {
-                outputNewCardsAndSetLastFetched(data, boardId, listId)
-              }
-            }
-        );
-      } else {
-        trello.get('/1/members/me/cards', (err, data) => {
-          if (err) {
-            node.error(err)
-          } else {
-            var newIdSet = new Set();
-
-            for (let i = 0; i < data.length; i++) {
-              newIdSet.add(data[i].id);
-            }
-            nodeContext.set(ID_ACCESS_STRING, newIdSet);
-          }
-        });
-      }
+      getTrelloCurrentUID();
     })
   }
 
