ace9af405a14688034019bb7320af6ca43803fbc
sean borg <sean_borg@hotmail.co.uk>
2019-01-18 13:00:28 +0000

    Added ability to exclude users actions on move list node

--- trelloGetCardMovedLists/trello-get-card-moved-lists.js
+++ trelloGetCardMovedLists/trello-get-card-moved-lists.js
@@ -10,11 +10,19 @@ module.exports = function (RED) {
     const credentialNode = RED.nodes.getNode(config.trello);
     const trello = new Trello(credentialNode.apikey, credentialNode.token);
     this.on('input', function (msg) {
+      //<editor-fold desc="Initialise all variables">
+      const trelloCurrentUserIDSaveString = 'trelloUID';
       const lastFetchedSaveString = 'lastFetched';
       const boardFetchesSaveString = 'boardFetches';
       const trelloData = msg.trello || {};
       const boardId = trelloData.idBoard || config.idBoard;
-
+      const excludeSelfCreated = config.excludeUser;
+      let trelloCurrentUserID = '';
+      try {
+        trelloCurrentUserID = nodeContext.get(trelloCurrentUserIDSaveString);
+      } catch (e) {
+        node.error(e)
+      }
       let lastFetched = '';
       try {
         lastFetched = nodeContext.get(lastFetchedSaveString);
@@ -31,22 +39,102 @@ module.exports = function (RED) {
       if (!boardFetches) {
         boardFetches = {};
       }
+      if (!lastFetched) {
+        if (msg.payload.initialTimeStamp) {
+          lastFetched = msg.payload.initialTimeStamp;
+          node.warn('Used initialTimeStamp input to set the starting point. This could cause some duplication.');
+        }
+      }
+      //</editor-fold>
 
+      // pre check that we have the current user ID associated with the api key, if not we shall go fetch it.
+      // TODO I may want to set a flag if the credentials have changed as that would change the user
+      function getTrelloCurrentUID() {
+        if (trelloCurrentUserID) {
+          main();
+        } else {
+          trello.get('/1/members/me', (err, data) => {
+            if (err) {
+              node.error(err)
+            } else {
+              trelloCurrentUserID = data.id;
+              nodeContext.set(trelloCurrentUserIDSaveString, trelloCurrentUserID); // store for faster access
+              main()
+            }
+          })
+        }
+      }
+
+      function main() {
+
+
+        if (lastFetched) {
+          const triggeredDate = new Date();
+
+          if (boardId) {
+            trello.get('/1/boards/' + boardId + '/actions', {since: lastFetched}, (err, data) => {
+                  if (err) {
+                    node.error(err)
+                  } else {
+                    outputNewCardsAndSetLastFetched(data, lastFetched, triggeredDate);
+                    setLastFetched(triggeredDate, boardId);
+                    node.status({fill: "green", shape: "dot", text: triggeredDate.toISOString().substring(0, 19)});
+                  }
+                }
+            );
+          } else {
+            trello.get('/1/members/me/boards', (err, data) => {
+                  if (err) {
+                    node.error(err)
+                  } else {
+                    for (let i = 0; i < data.length; i++) {
+                      let fetchDatetime = lastFetched;
+                      if (boardFetches && boardFetches[data[i].id]) {
+                        fetchDatetime = boardFetches[data[i].id];
+                        if (fetchDatetime !== lastFetched) {
+                          node.log('Attempting to recover any missed card movements from ' + data[i].name + ': ' + data[i].id + ' since ' + fetchDatetime)
+                        }
+                      }
+
+
+                      trello.get('/1/boards/' + data[i].id + '/actions', {since: fetchDatetime}, (err, data2) => {
+                            if (err) {
+                              node.error('Failed to retrieve board ' + data[i].name + ': ' + data[i].id + ' shall attempt to recover any missed card movements when next triggered')
+                            } else {
+                              outputNewCardsAndSetLastFetched(data2, lastFetched, triggeredDate);
+                              setLastFetched(triggeredDate, data[i].id);
+                              node.status({fill: "green", shape: "dot", text: triggeredDate.toISOString().substring(0, 19)});
+                            }
+                          }
+                      );
+                    }
+                  }
+                }
+            );
+          }
+        } else {
+          setLastFetched(new Date(), null);
+        }
+      }
 
       function outputNewCardsAndSetLastFetched(actionData) {
         for (let i = 0; i < actionData.length; i++) {
           if (actionData[i].data) {
             if (actionData[i].type === 'updateCard') {
-              // Bellow looks for any action data that contains a listAfter and list before key signifying a card has been moved
-              if (actionData[i].data.listAfter && actionData[i].data.listBefore) {  // Is it best to just check for undefined?
-                trello.get('/1/cards/' + actionData[i].data.card.id, (err, data) => {
-                      if (err) {
-                        node.error(err)
-                      } else {
-                        node.send({payload: data});
+              // Bellow will test if excludeSelfCreated is set, if it wasn't it will just continue, if it was it will
+              // check that the user that created the card is not the user who's linked to (owns) the API key
+              if (!excludeSelfCreated || !(actionData[i].idMemberCreator === trelloCurrentUserID)) {
+                // Bellow looks for any action data that contains a listAfter and list before key signifying a card has been moved
+                if (actionData[i].data.listAfter && actionData[i].data.listBefore) {  // Is it best to just check for undefined?
+                  trello.get('/1/cards/' + actionData[i].data.card.id, (err, data) => {
+                        if (err) {
+                          node.error(err)
+                        } else {
+                          node.send({payload: data});
+                        }
                       }
-                    }
-                );
+                  );
+                }
               }
             }
           }
@@ -63,61 +151,13 @@ module.exports = function (RED) {
         }
       }
 
-      if (!lastFetched) {
-        if (msg.payload.initialTimeStamp) {
-          lastFetched = msg.payload.initialTimeStamp;
-          node.warn('Used initialTimeStamp input to set the starting point. This could cause some duplication.');
-        }
-      }
 
 
-      if (lastFetched) {
-        const triggeredDate = new Date();
-
-        if (boardId) {
-          trello.get('/1/boards/' + boardId + '/actions', {since: lastFetched}, (err, data) => {
-                if (err) {
-                  node.error(err)
-                } else {
-                  outputNewCardsAndSetLastFetched(data, lastFetched, triggeredDate);
-                  setLastFetched(triggeredDate, boardId);
-                  node.status({fill: "green", shape: "dot", text: triggeredDate.toISOString().substring(0, 19)});
-                }
-              }
-          );
-        } else {
-          trello.get('/1/members/me/boards', (err, data) => {
-                if (err) {
-                  node.error(err)
-                } else {
-                  for (let i = 0; i < data.length; i++) {
-                    let fetchDatetime = lastFetched;
-                    if (boardFetches && boardFetches[data[i].id]) {
-                      fetchDatetime = boardFetches[data[i].id];
-                      if (fetchDatetime !== lastFetched) {
-                        node.log('Attempting to recover any missed card movements from ' + data[i].name + ': ' + data[i].id + ' since ' + fetchDatetime)
-                      }
-                    }
+      getTrelloCurrentUID();
 
 
-                    trello.get('/1/boards/' + data[i].id + '/actions', {since: fetchDatetime}, (err, data2) => {
-                          if (err) {
-                            node.error('Failed to retrieve board ' + data[i].name + ': ' + data[i].id + ' shall attempt to recover any missed card movements when next triggered')
-                          } else {
-                            outputNewCardsAndSetLastFetched(data2, lastFetched, triggeredDate);
-                            setLastFetched(triggeredDate, data[i].id);
-                            node.status({fill: "green", shape: "dot", text: triggeredDate.toISOString().substring(0, 19)});
-                          }
-                        }
-                    );
-                  }
-                }
-              }
-          );
-        }
-      } else {
-        setLastFetched(new Date(), null);
-      }
+
+
     })
   }
 
